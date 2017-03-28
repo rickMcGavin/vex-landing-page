bbbd8cf421478e8f2d9021677f0729f368406e96
rebelBeard <rfm144@gmail.com>
2017-03-27 23:19:57 -0400

    added normalize, global css, color variables, wrapper module

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,11 +2,7 @@ var gulp = require('gulp'),
 sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-	gulp.src('./app/assets/styles/**/*.scss')
+	return gulp.src('./app/assets/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(gulp.dest('./app/assets/styles'))
 });
-
-// gulp.task('watch', function() {
-// 	gulp.watch('./app/assets/styles/**/*.scss', ['styles']);
-// });
