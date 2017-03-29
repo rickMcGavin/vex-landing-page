7df87bf8a73db70355f7435cd428ad62e7aba0f8
rebelBeard <rfm144@gmail.com>
2017-03-28 20:47:06 -0400

    completed standard view of above fold area

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/**/*.scss')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
 		.pipe(gulp.dest('./app/assets/styles'))
 });
